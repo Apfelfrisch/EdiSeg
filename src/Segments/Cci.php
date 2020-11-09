4009b77e74bc5916cb10ce682051963a4144eb5d
Nils Refle <n.refle@proengeno.de>
2020-11-09 12:56:46 +0100

    feat: Adde Attribute for CCI

--- src/Segments/Cci.php
+++ src/Segments/Cci.php
@@ -13,13 +13,13 @@ class Cci extends AbstractSegment
         'C240' => ['7037' => 'O|an|17', '1131' => null, '3055' => null, '7036' => 'O|an|35'],
     ];
 
-    public static function fromAttributes($type, $code, $mark = null)
+    public static function fromAttributes($type, $code, $mark = null, $codeList = null, $codeResponsible = null)
     {
         return new static([
             'CCI' => ['CCI' => 'CCI'],
             '7059' => ['7059' => $type],
             'C502' => ['6313' => null],
-            'C240' => ['7037' => $code, '1131' => null, '3055' => null, '7036' => $mark],
+            'C240' => ['7037' => $code, '1131' => $codeList, '3055' => $codeResponsible, '7036' => $mark],
         ]);
     }
 
@@ -31,6 +31,16 @@ class Cci extends AbstractSegment
     public function code()
     {
         return $this->elements['C240']['7037'];
+    }
+
+    public function codeList()
+    {
+        return $this->elements['C240']['1131'];
+    }
+
+    public function codeResponsible()
+    {
+        return $this->elements['C240']['3055'];
     }
 
     public function mark()
